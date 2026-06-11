80ab5e0e107df52d2ba5a9631cad5cad358ec22b
ali3khalid-dot <ali3khalid@gmail.com>
2026-06-11 12:04:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import asyncio
 
+print("🔥 Kakashi AI System Started 🔥")
+
+async def main():
+    while True:
+        print("👁️ System Running...")
+        await asyncio.sleep(5)
+
+if __name__ == "__main__":
+    asyncio.run(main())
